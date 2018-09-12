977b59e69cef4901b5689d896f0522780e1bd877
Evan <20EyringEvan@bprep.org>
2018-09-12 10:12:09 -0700

    school copy

---
+++ 6 Days of Christmas/src/DaysOfChristmas.java
@@ -0,0 +1,64 @@
+public class DaysOfChristmas {
+    public static void main(String[] args) {
+
+        Day1();
+        Day2();
+        Day3();
+
+    }
+
+    public static void Day1() {
+        System.out.println("On the 1st day of \"Xmas\", my true love sent to me");
+        partridge();
+        System.out.println();
+}
+    public static void partridge() {
+        System.out.println("a partridge in a pear tree");
+    }
+
+
+    public static void Day2() {
+        System.out.println("On the 2nd day of \"Xmas\", my true love sent to me");
+        turtleDoves();
+        System.out.println();
+    }
+
+    public static void turtleDoves() {
+        System.out.println("two turtle doves, and");
+        partridge();
+    }
+
+    public static void Day3() {
+        System.out.println("On the 3rd day of \"Xmas\", my true love sent to me");
+        frenchHens();
+
+    }
+
+    public static void frenchHens() {
+        System.out.println("three french hens");
+        turtleDoves();
+
+    }
+
+    public static void Day4() {
+        System.out.println("On the 4th day of \"Xmas\", my true love sent to me");
+        callingBirds();
+
+    }
+
+    public static void callingBirds() {
+        System.out.println("four calling birds");
+        frenchHens();
+
+    }
+
+    public static void Day5() {
+        System.out.println("On the 5th day of \"Xmas\", my true love sent to me");
+
+    }
+
+    public static void goldenRings() {
+
+    }
+
+}
