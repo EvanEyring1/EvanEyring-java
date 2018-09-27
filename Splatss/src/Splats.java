09b9314ef89914e07109256775297574cbfa9139
Evan <20EyringEvan@bprep.org>
2018-09-27 09:20:47 -0700

    work

---
+++ Splatss/src/Splats.java
@@ -0,0 +1,11 @@
+public class Splats {
+    public static final int SIZE=40;
+    public static void main(String[] args) {
+    stars();
+    }
+
+    public static void stars() {
+        for (int i=1; i<= SIZE;i++);
+        System.out.println("********************");
+    }
+}
