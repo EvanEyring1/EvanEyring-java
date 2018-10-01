f29d956d06f833943a913f1cc68558e0270655d7
Evan <20EyringEvan@bprep.org>
2018-10-01 09:21:01 -0700

    ork

---
+++ RocketShip/src/Rocket.java
@@ -0,0 +1,9 @@
+public class Rocket {
+    public static final int SIZE = 3;
+
+    public static void main(String[] args) {
+    }
+}
+    public static void drawCone() {
+        for (int line = 1; line <= (SIZE * 2 - 1); line++) {
+    }
