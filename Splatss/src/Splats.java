d238e6e272dc51d47ee459d57b85fa249922004d
Evan <20EyringEvan@bprep.org>
2018-10-01 09:10:27 -0700

    work

--- Splatss/src/Splats.java
+++ Splatss/src/Splats.java
@@ -1,11 +1,44 @@
 public class Splats {
-    public static final int SIZE=40;
+    public static final int ROW = 6;
+
     public static void main(String[] args) {
-    stars();
+        stars();
+        dollar();
+        hashtag();
+
     }
 
     public static void stars() {
-        for (int i=1; i<= SIZE;i++);
-        System.out.println("********************");
+        for (int i = 0; i <= ROW; i++) {
+            for (int j = 0; j < ROW; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void dollar() {
+        for (int i = 0; i <= ROW; i++) {
+            for (int j = 0; j < ROW; j++) {
+                System.out.print("$");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+
+
+    public static void hashtag() {
+        for (int i = 0; i <= ROW; i++) {
+            for (int j = 0; j < ROW; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
     }
 }
+
+
+
