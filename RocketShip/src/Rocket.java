f2d62bcfc6aec1253e2a17c367c176681a58897b
Evan <20EyringEvan@bprep.org>
2018-10-03 09:57:34 -0700

    Final

--- RocketShip/src/Rocket.java
+++ RocketShip/src/Rocket.java
@@ -1,9 +1,82 @@
+//Evan Eyring
+//10/3/18
+//AP COMP SCIENCE A
+//Lab 2: Rocketship
+//
 public class Rocket {
-    public static final int SIZE = 3;
+    public static final int SIZE = 3;        /* SIZE is the class constant, if this number changes, so does the rocket's scale */
 
     public static void main(String[] args) {
+        drawCone();
+        drawDivide();
+        drawTop();
+        drawBot();
+        drawDivide();
+        drawBot();
+        drawTop();
+        drawDivide();
+        drawCone();
+    }
+
+    public static void drawCone() {                                         /*Method genrates the cone through nested for loops */
+        for (int line = 1; line <= (SIZE * 2 - 1); line++) {
+            System.out.print(" ");
+            for (int space = 1; space <= (SIZE * 2 - 1) - line; space++) {
+                System.out.print(" ");
+            }
+            for (int slashes = 1; slashes <= line; slashes++) {
+                System.out.print("/");
+            }
+            System.out.print("**");
+            for (int slashes = 1; slashes <= line; slashes++) {
+                System.out.print("\\");
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public static void drawDivide() {
+        System.out.print("+");
+        for (int i = 1; i <= SIZE * 2; i++) {
+            System.out.print("=*");
+        }
+        System.out.println("+");
+    }
+
+    public static void drawTop() {                                  /*Generates the top part of the rocket through nested and normal for loops */
+            for (int line = 1; line <= SIZE; line++) {
+                System.out.print("|");
+            for (int repeat = 1; repeat <= 2; repeat++) {
+                for (int dots = 1; dots <= SIZE - line; dots++) {
+                System.out.print(".");
+                }
+                for (int point = 1; point <= line; point++) {
+                    System.out.print("/\\");
+                }
+                for (int dots = 1; dots <= SIZE - line; dots++) {
+                    System.out.print(".");
+                }
+            }
+            System.out.println("|");
+        }
+    }
+
+     public static void drawBot() {                             /*Method generates the bottom portion of the rocket through nested and normal for loops*/
+        for (int line = SIZE; line >= 1; line--) {
+            System.out.print("|");
+            for (int repeat = 1; repeat <= 2; repeat++) {
+                for (int dots = 1; dots <= SIZE - line; dots++) {
+                    System.out.print(".");
+                }
+                for (int point = 1; point <= line; point++) {
+                    System.out.print("\\/");
+                }
+                for (int dots = 1; dots <= SIZE - line; dots++) {
+                    System.out.print(".");
+                }
+            }
+            System.out.println("|");
+
+        }
     }
 }
-    public static void drawCone() {
-        for (int line = 1; line <= (SIZE * 2 - 1); line++) {
-    }
