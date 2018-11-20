dc07b20c07a2387204c0367ba9c7a11a05f92e73
Evan <20EyringEvan@bprep.org>
2018-11-20 09:17:42 -0800

    work copy

---
+++ Worksheet 4.1/src/ArrayWorksheet.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class ArrayWorksheet {
+    //1a)
+    public static void main(String[] args) {
+        int[] numbersArray1 = new int[8];
+        //2a
+        numbersArray1[0] = 10;
+        numbersArray1[1] = 9;
+        numbersArray1[2] = 8;
+        numbersArray1[3] = 7;
+        numbersArray1[4] = 6;
+        numbersArray1[5] = 5;
+        numbersArray1[6] = 4;
+        numbersArray1[7] = 3;
+        //2b
+        for (int i = 0; i < 8; i++) {
+            numbersArray1[i] = i * 3;
+        }
+        //2c
+        int[] numbersArray2 = new int[8];
+        for (int = 0; i <8; i++){
+            int[] numbersArray2 = {5, 10, 15, 20, 25};
+
+        }
+
+    }
+}
