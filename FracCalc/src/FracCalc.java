0fe5347b7c983dc016d20b34f1b0f5a65f626daf
Evan <20EyringEvan@bprep.org>
2019-01-04 09:14:46 -0800

    work copy 2

--- FracCalc/src/FracCalc.java
+++ FracCalc/src/FracCalc.java
@@ -8,22 +8,40 @@ public class FracCalc {
         System.out.print("Enter three tokens");
         while (!fInput.equals("quit)")) {
             String blah = "";
-            for (int i = 1; i <= 3; i++) {
-                for (int j = 0; j < fInput.length(); j++) {
+            for (int j = 1; j <= 3; j++) {
+                for (int i = 0; i < fInput.length(); i++) {
                     if (fInput.charAt(j) != ' ') {
                         blah += blah + fInput.charAt(j);
                     } else {
                     }
-
+                    while (!fInput.equals("quit")) {
+                        String [] terms = new String[3];
+                        int x = -1;
+                        int m = 0;
+                        for (int y = 0; y < fInput.length() ;
+                        y++){
+                            x++;
+                            if (fInput.charAt(y) == ' ') {
+                                System.out.println(fInput.substring(y - x, y));
+                                terms[m] = fInput.substring(y - x, y);
+                                x = -1;
+                                m++;
+                            }
+                            if (m == 2) {
+                                System.out.println(fInput.substring(y + 1));
+                                terms[m] = fInput.substring(y + 1);
+                                m++;
+                                //Word count is increasing
+                            }
+                            System.out.println(blah);
+                        }
+                        System.out.print("Input again:");
+                        fInput = input.nextLine();
+                    }
                 }
-                System.out.println(blah);
             }
-            System.out.print("Input again");
-            fInput = input.nextLine();
 
+                    }
+                }
+            }
 
-        }
-    }
-
-
-    }
